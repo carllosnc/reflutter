63ea3fb887eeae1aefe733f7614e7900e775eafc
Carlos Costa <carllos.nc@gmail.com>
2024-08-09 11:13:30 -0300

    Rename

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 import 'examples/stateless_widgets/stateless_widgets_page.dart';
-import 'examples/stateful_components/stateful_widgets_page.dart';
+import 'examples/stateful_widgets/stateful_widgets_page.dart';
 import 'examples/shared_state/share_state_page.dart';
 
 void main() {
