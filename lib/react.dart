89270f18b9d229c68f36b5410f6630641a4480a6
Carlos Costa <carllos.nc@gmail.com>
2024-08-09 10:19:26 -0300

    Adding RenderGlobal and RenderGlobal

--- lib/react.dart
+++ lib/react.dart
@@ -8,16 +8,27 @@ class useState extends ValueNotifier {
     addListener(callback);
   }
 
+  removeEffect(Function() callback) {
+    removeListener(callback);
+  }
+
   setState(Function(dynamic value) callback) {
     value = callback(value);
     super.value = value;
   }
 }
 
-class Render extends ValueListenableBuilder {
-  const Render({
+class RenderGlobal extends ValueListenableBuilder {
+  const RenderGlobal({
     super.key,
     required super.valueListenable,
+    required super.builder,
+  });
+}
+
+class RenderLocal extends StatefulBuilder {
+  const RenderLocal({
+    super.key,
     required super.builder,
   });
 }
